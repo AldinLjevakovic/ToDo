a3b12e4e6b45d4c3208e64e7719b668a7bcad041
AldinLjevakovic <95365811+AldinLjevakovic@users.noreply.github.com>
2024-06-18 23:09:06 +0200

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -12,6 +12,6 @@ CREATE TABLE `tasks` (
   `task` varchar(100) NOT NULL,
   `done` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`taskid`),
-  -- KEY `username_idx` (`username`),
+  
   CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 );
